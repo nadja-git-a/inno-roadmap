e3e57aa5c6010b633f3c068b6f6e8050e0bca5c2
nadezhda-alesenko <nadezhda.alesenko.work@gmail.com>
2025-07-16 10:17:45 +0300

    set up webpack with ESLint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      js,
+      prettier: prettierPlugin,
+    },
+    extends: [
+      'js/recommended',
+      'plugin:prettier/recommended', 
+    ],
+    rules: {
+      'prettier/prettier': 'error', 
+    },
+  },
+]);
+
+
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    open: true,
+    hot: true,
+    port: 3000,
+  },
+};
+
